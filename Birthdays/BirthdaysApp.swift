0df6a854512256e4f072cb7179e0712323e3f383
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-18 23:42:09 +0200

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Jean on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthdays/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Jean on 18/10/24.
+//
+
+import Foundation
+
+struct Friend {
+    var name: String
+    var birthday: Date
+}
